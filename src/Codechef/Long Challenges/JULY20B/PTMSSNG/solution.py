2bcca5e8e97b7284eb1c29ac76611aafad772481
dschinzo <ds.chinzo@gmail.com>
2020-07-06 16:42:23 +0800

    done

--- src/Codechef/Long Challenges/JULY20B/PTMSSNG/solution.py
+++ src/Codechef/Long Challenges/JULY20B/PTMSSNG/solution.py
@@ -1,16 +1,17 @@
 # cook your dish here
 for t in range(int(input())):
     n = int(input())
-    x_cor = []
-    y_cor = []
+    x_cor = {}
+    y_cor = {}
     for _ in range(4*n - 1):
         x, y = map(int, input().split())
         if x in x_cor:
-            x_cor.remove(x)
+            x_cor.pop(x)
         else:
-            x_cor.append(x)
+            x_cor[x] = 1
         if y in y_cor:
-            y_cor.remove(y)
+            y_cor.pop(y)
         else:
-            y_cor.append(y)
-    print(x_cor.pop(), y_cor.pop())
+            y_cor[y] = 1
+        
+    print(list(x_cor)[0], list(y_cor)[0])
