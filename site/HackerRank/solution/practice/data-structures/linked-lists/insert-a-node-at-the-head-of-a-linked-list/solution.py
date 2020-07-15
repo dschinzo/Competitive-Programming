97c3ddb860cd15e0a94caf693863ccb86792ca1e
dschinzo <ds.chinzo@gmail.com>
2020-07-16 00:14:26 +0800

    insert at tail

---
+++ site/HackerRank/solution/practice/data-structures/linked-lists/insert-a-node-at-the-head-of-a-linked-list/solution.py
@@ -0,0 +1,16 @@
+
+# Complete the insertNodeAtHead function below.
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+def insertNodeAtHead(llist, data):
+    # Write your code here
+    node = SinglyLinkedListNode(data)
+    node.next = llist
+    return node
